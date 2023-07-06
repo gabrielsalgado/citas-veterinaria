4a762203e56b6ca767ca797e4e9988546d1b7f11
Gabriel Salgado Ramírez <gabriel-salgado@outlook.com>
2023-07-06 14:47:43 -0600

    configuration files
    Adding original files

--- citas-veterinaria/tailwind.config.js
+++ citas-veterinaria/tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html","./src/**/*.jsx"],
+module.exports = {
+  purge: ["index.html", "./src/**/*.jsx"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+      extend: {},
+  },
+  variants: {
+      extend: {},
   },
   plugins: [],
-}
-
+};

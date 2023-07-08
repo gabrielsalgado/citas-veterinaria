1038a002f217692b3cbb14b4dc0bc71e85e72c4d
Gabriel Salgado Ramírez <gabriel-salgado@outlook.com>
2023-07-08 13:51:51 -0600

    fix tag

--- citas-veterinaria/src/components/ListadoPacientes.jsx
+++ citas-veterinaria/src/components/ListadoPacientes.jsx
@@ -4,7 +4,7 @@ const  ListadoPacientes =() => {
     <div>
               <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
             Listado Pacientes {""} 
-            <span className="text-indigo-700">V/span>
+            <span className="text-indigo-700">V</span>
         </h1>
     </div>
   )

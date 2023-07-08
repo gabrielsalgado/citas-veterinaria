02d5b926fc909a459a100590238aa78306c8c312
Gabriel Salgado Ramírez <gabriel-salgado@outlook.com>
2023-07-08 13:10:32 -0600

    update

--- citas-veterinaria/App.jsx
+++ citas-veterinaria/App.jsx
@@ -1,10 +1,16 @@
+
 import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
+
   return (
-    <div className="container mx-auto mt-20">
+    <div>
       <Header />
+      <Formulario/>
+      <ListadoPacientes/>
 
 
     </div>
